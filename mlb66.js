9ea49dd6cd434aa977d484802d1007686000da20
tamales <37490316+billiumgates@users.noreply.github.com>
2022-04-11 00:21:08 -0600

    Update version for TamperMonkey

--- mlb66.js
+++ mlb66.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MLB66Fix
 // @namespace    https://mlb66.ir
-// @version      0.4.9
+// @version      0.5.0
 // @description  Remove stupid shit
 // @author       billiumgates
 // @match        https://mlb66.ir
